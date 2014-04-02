b5e77016d1aaeb772cd4879fe3026b8c5339f05b
jorgeSV <amk114@gmail.com>
2014-04-02 17:59:45 +0200

    Pintar cuadro y onDraw.

--- LexDroidEjemploCanvas/src/com/sovejo/lexdroidejemplocanvas/MainActivity.java
+++ LexDroidEjemploCanvas/src/com/sovejo/lexdroidejemplocanvas/MainActivity.java
@@ -1,7 +1,15 @@
 package com.sovejo.lexdroidejemplocanvas;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 
 public class MainActivity extends Activity 
 {
@@ -10,7 +18,59 @@ public class MainActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		Vista vista = new Vista(this);
+		setContentView(vista);
+	}
+	
+	class Vista extends View
+	{
+		float x = 50;
+		float y = 50;
+		String accion = "Accion";
+		Path path = new Path();
+		
+		public Vista(Context context) 
+		{
+			super(context);
+			// TODO Auto-generated constructor stub
+		}
+		
+		public void onDraw(Canvas canvas)
+		{
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.STROKE);
+			paint.setStrokeWidth(10);
+			paint.setColor(Color.RED);
+			
+			int ancho = canvas.getWidth();
+			canvas.drawRect(20, 90, ancho - 20, 38, paint);
+			
+			if(accion == "Abajo")
+				path.moveTo(x, y);
+			
+			if(accion == "Mover")
+				path.lineTo(x, y);
+			
+			canvas.drawPath(path, paint);
+		}
+		
+		public boolean onTouchEvent(MotionEvent e)
+		{
+			x = e.getX();
+			y = e.getY();
+			
+			if(e.getAction() == MotionEvent.ACTION_DOWN)
+				accion = "Abajo";
+			
+			if(e.getAction() == MotionEvent.ACTION_MOVE)
+				accion = "Mover";
+				
+			invalidate();
+			return true;
+		}
 	}
 
 }
